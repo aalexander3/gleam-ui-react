6aeba789b762f576603eeffcefbf46370f1c6bec
Arren Alexander <aralx73@gmail.com>
2019-05-20 18:23:46 -0600

    notes for next

--- src/Headers/Header.js
+++ src/Headers/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = ({ text, color='default', className='', children, ...props }) => {
-
+  // add skew and highlight
   return (
     <h1 className={`header-${color} ${className}`} {...props} >{text} {children}</h1>
   )
--- src/Headers/SubHeader.js
+++ src/Headers/SubHeader.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const SubHeader = ({ text, size='medium', color='default', className='', children, ...props }) => {
+const SubHeader = ({ text, color='default', className='', children, ...props }) => {
+  // add skew and highlight
   return (
-    <h4 className={`header-${size} header-${color} ${className}`} {...props} >{text} {children}</h4>
+    <h4 className={`header-${color} ${className}`} {...props} >{text} {children}</h4>
   )
 }
 
