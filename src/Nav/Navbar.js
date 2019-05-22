b5e26121fe9577dd4f93681566b4a6b017703bf8
Arren Alexander <aralx73@gmail.com>
2019-05-22 16:32:00 -0600

    fix: added media queries for navbar

--- src/Nav/Navbar.js
+++ src/Nav/Navbar.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Media from "react-media";
+import NavGroup from './NavGroup'
 
 class Navbar extends Component {
   state = {
     open: false
   }
 
+  getHamburger = () => {
+    return (
+      <div className='nav-item' onClick={this.toggleCollapsed} >
+        HAMBURGER
+      </div>
+    )
+  }
+
+  toggleCollapsed = () => {
+    this.setState(prevState => ({ open: !prevState.open }))
+  }
+
   render(){
     const { children, direction, color } = this.props
-    const classes = `navbar navbar-${direction} ${color}`
+    const { open } = this.state
+    const classes = open ? `navbar navbar-${direction} open ${color}` : `navbar navbar-${direction} ${color}`
 
     return (
-      <nav className={classes} >
-        {children}
-      </nav>
+      <Fragment>
+        <Media query="(max-width: 650px)" >
+          <Fragment>
+            <nav className={classes} >
+              <NavGroup justify='right' >{this.getHamburger()}</NavGroup>
+              {open &&
+                <div className='extended-nav' >
+                  {children}
+                </div>
+              }
+            </nav>
+          </Fragment>
+        </Media>
+        <Media query="(min-width: 651px)" >
+          <nav className={classes} >
+            {children}
+          </nav>
+        </Media>
+      </Fragment>
     )
   }
 }
